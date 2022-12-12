dd6b682f564d00a17c78c2d61018764d16c34e83
rmonteiro46 <rmonteiro46@gmail.com>
2022-12-12 01:22:40 +0000

    sort array by property

--- sandBox.js
+++ sandBox.js
@@ -1,5 +1,79 @@
-console.log("hello");
-
-
-console.log("hello2");
-console.log("hello3");
+const list = [
+    {
+      name: "Rafi Backe",
+      jobTitle: "Research Assistant IV",
+      age: 41
+    },
+    {
+      name: "Gottfried Balser",
+      jobTitle: "Mechanical Systems Engineer",
+      age: 55
+    },
+    {
+      name: "Lauraine Nore",
+      jobTitle: "Assistant Media Planner",
+      age: 61
+    },
+    {
+      name: "Allayne Burstow",
+      jobTitle: "Help Desk Technician",
+      age: 48
+    },
+    {
+      name: "Alan Tumber",
+      jobTitle: "Senior Developer",
+      age: 23
+    },
+    {
+      name: "Carlyn Urrey",
+      jobTitle: "Operator",
+      age: 59
+    },
+    {
+      name: "Kerrin Anstee",
+      jobTitle: "Staff Accountant III",
+      age: 38
+    },
+    {
+      name: "Hube Tibbles",
+      jobTitle: "VP Product Management",
+      age: 41
+    },
+    {
+      name: "Niki Core",
+      jobTitle: "Engineer I",
+      age: 18
+    },
+    {
+      name: "Guntar Shailer",
+      jobTitle: "Accounting Assistant I",
+      age: 57
+    },
+    {
+      name: "Sophi Brophy",
+      jobTitle: "Senior Financial Analyst",
+      age: 48
+    },
+    {
+      name: "Arluene McRoberts",
+      jobTitle: "Business Systems Development Analyst",
+      age: 51
+    },
+    {
+      name: "Belvia Fearnehough",
+      jobTitle: "Paralegal",
+      age: 50
+    }
+  ];
+  
+  console.log(list);
+  //console.log(list.length);
+  
+  const sortArrayFunc = (val1, val2) => {
+    return val1.age - val2.age;
+  };
+  
+  const listSortedByAge = list.sort(sortArrayFunc);
+  
+  console.log(listSortedByAge);
+  
